becb81af5956f3f275b889d67f4ae92c322896ba
Everett Grethel <everett.grethel@gmail.com>
2021-03-25 17:10:15 -0400

    changed routing to single app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <cocktailsearch v-on:search-item="searchItem" />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import cocktailsearch from '@/components/CocktailSearch.vue'
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: { cocktailsearch },
+  data: function() {
+    return {
+      item: ""
+    }
+  },
+  methods: {
+    searchItem: function(item) {
+      console.log("hello!");
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${item}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+    },
+  },
+};
+</script> 
---
+++ src/components/CocktailSearch.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="home">
+    <h1>Cocktail Search!</h1>
+    <form class="searchForm" @submit.prevent="searchItem">
+      <p>
+          <label for="item">Search:</label>
+          <input v-model="item">
+      </p>
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cocktailsearch',
+  data: function() {
+    return {
+      item: ""
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+      searchItem: function() {
+          this.$emit('search-item', this.item);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+
+</style>
